095c444b6ffa206025c9716c7d5b6cddbc0c98b8
Ludwing Hernandez <Ludwing.hernandez.71653@gmal.com>
2022-02-27 15:34:42 -0600

    Practica

---
+++ clonerepository/clonerepository/componentesgrupoa22/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">    
+    <img alt="Vue logo" src="./assets/logo.png">
+    <header-title title="Programacion Computacional IV"/>
+    <HeaderTitle/>
+  </div>
+</template>
+
+<script>
+
+import Tienda_online from '../../../../tienda_online/src/components/Tienda_online.vue'
+import HeaderTitle from './components/HeaderTitle.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderTitle,
+    Tienda_online
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ clonerepository/clonerepository/componentesgrupoa22/src/components/ProductList.vue
@@ -0,0 +1,22 @@
+<template>
+    
+    <div id = "root">
+        <h2>Productos</h2>
+
+    </div>
+
+</template>
+
+<script>
+    import products from '../data/products.json'
+
+export default {
+    name: 'ProductList',
+    data(){
+        return{
+            items: products
+        }
+    }
+}
+</script>
+
